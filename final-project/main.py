d34288c1927821842e40e8396f7d37157a976920
Michael Marsico <michaelrmarsico@icloud.com>
2023-12-14 10:11:10 -0600

    Remove Unused Imports

--- final-project/main.py
+++ final-project/main.py
@@ -1,6 +1,3 @@
-import pandas as pd
-import numpy as np
-import pandas as pd
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
